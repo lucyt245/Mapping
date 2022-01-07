3c5b47ecc4c2b45e24d3f2a57f2d69efcbfcd561
Lucy Taylor <77295077+lucyt245@users.noreply.github.com>
2022-01-07 20:36:03 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import folium
+>>> map = folium.Map
+>>> map = folium.Map(location = [51.5, -0.13])
+>>> map.save('firstMap.html')
+>>> map = folium.Map(location = [51.5, -0.13], zoom_start = 4)
+>>> map.save('firstMap.html')
+>>> map = folium.Map(location = [51.5, -0.13], zoom_start = 5)
+>>> map.save('firstMap.html')
+>>> map = folium.Map(location = [54, -4.4], zoom_start = 6)
+>>> map.save('firstMap.html')
+>>> map = folium.Map(location = [56. -4-4], zoom_start = 6)
+>>> map.save('firstMap.html')
+>>> map = folium.Map(location = [56, -4.4], zoom_start = 6)
+>>> map.save('firstMap.html')
+>>> map = folium.Map(location = [55, -4.4], zoom_start = 6)
+>>> map.save('firstMap.html')
