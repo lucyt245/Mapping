020edd2cde74f3dfa170eb0a028d15433dbfe695
Lucy Taylor <77295077+lucyt245@users.noreply.github.com>
2022-01-08 12:51:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import folium
+import pandas
 
 map = folium.Map(location = [55, -4.4], zoom_start = 6, tiles = 'Stamen Terrain')
+data = pandas.read_csv('volcanoes.txt')
 
 fg = folium.FeatureGroup(name = 'MyMap')
 
-for coordinates in [[31, 5],[50, 10]]:
-    fg.add_child(folium.Marker(location = coordinates, popup = 'marker', icon = folium.Icon(color = 'purple')))
+lat = list(data['LAT'])
+lon = list(data['LON'])
+volc_name = list(data['NAME'])
+location = list(data['LOCATION'])
+
+for lt, ln, nm, loc in zip(lat, lon, volc_name, location):
+    loc = loc[3:]
+    # gets rid of the 'US-' part of the location
+    marker = nm + ', ' + loc
+    fg.add_child(folium.Marker(location = [lt, ln], popup = marker, icon = folium.Icon(color = 'red')))
 
 map.add_child(fg)
 
